7b339b3c06c9f09747f68cf9eff6013ef92b04e9
Rahma <rahma.malkany@gmail.com>
2019-02-12 15:26:38 +0100

    Commit

---
+++ Hello.java
@@ -0,0 +1,10 @@
+/**First Commit**/
+public class Hello{
+	public static void main(String [] args){
+		int a=5;
+		String fichier="Readme.txt";
+		System.out.println("Hello world!");
+		System.out.println("a="+ a);
+}
+
+}
